f18a40ce0538d36117745ae2b9720464f5714f06
John Lindquist <johnlindquist@gmail.com>
2018-10-23 16:41:43 -0600

    kill

--- dist/greensock-lessons.1100ee73.js
+++ dist/greensock-lessons.1100ee73.js
@@ -8940,184 +8940,31 @@ exports._gsScope = _TweenLite._gsScope;
 
 var _gsap = require("gsap");
 
-_gsap.TweenMax.set("#box", { xPercent: -50, yPercent: -50 });
+var divs = Array.from({ length: 100 }, function () {
+  return document.createElement("div");
+});
+
+divs.forEach(function (div) {
+  _gsap.TweenMax.set(div, {
+    position: "absolute",
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight,
+    width: 20,
+    height: 20,
+    //centering
+    xPercent: -50,
+    yPercent: -50,
+    backgroundColor: "green",
+    border: "3px solid black"
+  });
+
+  document.body.appendChild(div);
+});
+
+_gsap.TweenMax.to(divs, 10, { x: 100, y: 100 });
 
 document.addEventListener("click", function (event) {
-  var clientX = event.clientX,
-      clientY = event.clientY;
-
-
-  _gsap.TweenMax.to("#box", 1, { x: clientX, y: clientY });
+  _gsap.TweenMax.killAll(true);
 });
-},{"gsap":"node_modules/gsap/index.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
-var global = arguments[3];
-var OVERLAY_ID = '__parcel__error__overlay__';
-
-var OldModule = module.bundle.Module;
-
-function Module(moduleName) {
-  OldModule.call(this, moduleName);
-  this.hot = {
-    data: module.bundle.hotData,
-    _acceptCallbacks: [],
-    _disposeCallbacks: [],
-    accept: function (fn) {
-      this._acceptCallbacks.push(fn || function () {});
-    },
-    dispose: function (fn) {
-      this._disposeCallbacks.push(fn);
-    }
-  };
-
-  module.bundle.hotData = null;
-}
-
-module.bundle.Module = Module;
-
-var parent = module.bundle.parent;
-if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
-  var hostname = '' || location.hostname;
-  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '64481' + '/');
-  ws.onmessage = function (event) {
-    var data = JSON.parse(event.data);
-
-    if (data.type === 'update') {
-      console.clear();
-
-      data.assets.forEach(function (asset) {
-        hmrApply(global.parcelRequire, asset);
-      });
-
-      data.assets.forEach(function (asset) {
-        if (!asset.isNew) {
-          hmrAccept(global.parcelRequire, asset.id);
-        }
-      });
-    }
-
-    if (data.type === 'reload') {
-      ws.close();
-      ws.onclose = function () {
-        location.reload();
-      };
-    }
-
-    if (data.type === 'error-resolved') {
-      console.log('[parcel] ✨ Error resolved');
-
-      removeErrorOverlay();
-    }
-
-    if (data.type === 'error') {
-      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
-
-      removeErrorOverlay();
-
-      var overlay = createErrorOverlay(data);
-      document.body.appendChild(overlay);
-    }
-  };
-}
-
-function removeErrorOverlay() {
-  var overlay = document.getElementById(OVERLAY_ID);
-  if (overlay) {
-    overlay.remove();
-  }
-}
-
-function createErrorOverlay(data) {
-  var overlay = document.createElement('div');
-  overlay.id = OVERLAY_ID;
-
-  // html encode message and stack trace
-  var message = document.createElement('div');
-  var stackTrace = document.createElement('pre');
-  message.innerText = data.error.message;
-  stackTrace.innerText = data.error.stack;
-
-  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
-
-  return overlay;
-}
-
-function getParents(bundle, id) {
-  var modules = bundle.modules;
-  if (!modules) {
-    return [];
-  }
-
-  var parents = [];
-  var k, d, dep;
-
-  for (k in modules) {
-    for (d in modules[k][1]) {
-      dep = modules[k][1][d];
-      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
-        parents.push(k);
-      }
-    }
-  }
-
-  if (bundle.parent) {
-    parents = parents.concat(getParents(bundle.parent, id));
-  }
-
-  return parents;
-}
-
-function hmrApply(bundle, asset) {
-  var modules = bundle.modules;
-  if (!modules) {
-    return;
-  }
-
-  if (modules[asset.id] || !bundle.parent) {
-    var fn = new Function('require', 'module', 'exports', asset.generated.js);
-    asset.isNew = !modules[asset.id];
-    modules[asset.id] = [fn, asset.deps];
-  } else if (bundle.parent) {
-    hmrApply(bundle.parent, asset);
-  }
-}
-
-function hmrAccept(bundle, id) {
-  var modules = bundle.modules;
-  if (!modules) {
-    return;
-  }
-
-  if (!modules[id] && bundle.parent) {
-    return hmrAccept(bundle.parent, id);
-  }
-
-  var cached = bundle.cache[id];
-  bundle.hotData = {};
-  if (cached) {
-    cached.hot.data = bundle.hotData;
-  }
-
-  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
-    cached.hot._disposeCallbacks.forEach(function (cb) {
-      cb(bundle.hotData);
-    });
-  }
-
-  delete bundle.cache[id];
-  bundle(id);
-
-  cached = bundle.cache[id];
-  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
-    cached.hot._acceptCallbacks.forEach(function (cb) {
-      cb();
-    });
-    return true;
-  }
-
-  return getParents(global.parcelRequire, id).some(function (id) {
-    return hmrAccept(global.parcelRequire, id);
-  });
-}
-},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
+},{"gsap":"node_modules/gsap/index.js"}]},{},["index.js"], null)
 //# sourceMappingURL=/greensock-lessons.1100ee73.map
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 import { TweenMax } from "gsap"
 
-TweenMax.set("#box", { xPercent: -50, yPercent: -50 })
+const divs = Array.from({ length: 100 }, () =>
+  document.createElement("div")
+)
+
+divs.forEach(div => {
+  TweenMax.set(div, {
+    position: "absolute",
+    x: Math.random() * window.innerWidth,
+    y: Math.random() * window.innerHeight,
+    width: 20,
+    height: 20,
+    //centering
+    xPercent: -50,
+    yPercent: -50,
+    backgroundColor: "green",
+    border: "3px solid black"
+  })
+
+  document.body.appendChild(div)
+})
+
+TweenMax.to(divs, 10, { x: 100, y: 100 })
 
 document.addEventListener("click", event => {
-  const { clientX, clientY } = event
-
-  TweenMax.to("#box", 1, { x: clientX, y: clientY })
+  TweenMax.killAll(true)
 })
